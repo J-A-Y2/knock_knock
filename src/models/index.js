1d0df0b752c38577f1e5a2a9db5391a4dfc6b978
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-07-14 00:25:32 +0900

    feaure: User스키마 작성 완료

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,6 @@
 import Sequelize from "sequelize";
 import Config from "../config/config.json" assert { type: "json" };
-
+import User from "./user.js";
 const env = process.env.NODE_ENV || "development";
 const config = Config[env];
 const db = {};
@@ -10,11 +10,15 @@ const sequelize = new Sequelize(
   config.password,
   config
 );
+
+db.User = User(sequelize, Sequelize);
 Object.keys(db).forEach((modelName) => {
+  // associate 호출
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
