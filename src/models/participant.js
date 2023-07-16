834362b16ad886757b2983abff4c559129686754
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-07-16 15:05:56 +0900

    Feat: paricipant associate 연결 완료

--- src/models/participant.js
+++ src/models/participant.js
@@ -26,7 +26,16 @@ const Participant = (sequelize, DataTypes) => {
     }
   );
   Participant.associate = (db) => {};
-
+  // foreignKey는 Participant모델의 userId, targetKey는 User 모델의 userId
+  db.Participant.belongsTo(db.User, {
+    foreignKey: "userId",
+    targetKey: "userId",
+  });
+  // foreignKey는 Participant모델의 postId, targetKey는 Post 모델의 postId
+  db.Participant.belongsTo(db.Post, {
+    foreignKey: "postId",
+    targetKey: "postId",
+  });
   return Participant;
 };
 
