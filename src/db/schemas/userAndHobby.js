53e895aa7beb90b642b43f2354d289a5734c5938
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-07-17 02:34:28 +0900

    Feat: userAndHobby 스키마 작성

---
+++ src/db/schemas/userAndHobby.js
@@ -0,0 +1,38 @@
+const UserAndHobby = (sequelize, DataTypes) => {
+  const UserAndHobby = sequelize.define(
+    "UserAndHobby",
+    {
+      hobbyId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      hobbyType: {
+        type: DataTypes.STRING(15),
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      underscored: true,
+      modelName: "UserAndHobby",
+      tableName: "userAndHobbies",
+      paranoid: false,
+    }
+  );
+  UserAndHobby.associate = (db) => {
+    db.UserAndHobby.belongsTo(db.User, {
+      foreignKey: "userId",
+      targetKey: "userId",
+    }); // foreignKey는 UserAndHobby 모델의 userId, targetKey는 User 모델의 userId
+    db.UserAndHobby.belongsTo(db.Hobby, {
+      foreignKey: "hobbyId",
+      targetKey: "hobbyId",
+    }); // foreignKey는 UserAndHobby 모델의 hobbyId, targetKey는 Hobby 모델의 hobbyId
+  };
+
+  return UserAndHobby;
+};
+
+export default UserAndHobby;
