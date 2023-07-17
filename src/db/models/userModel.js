5f0948a4efccb435240c5f794c5e9534292a1157
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-07-17 23:27:42 +0900

    Feat: updateUser, deleteUser 모델 작성

--- src/db/models/userModel.js
+++ src/db/models/userModel.js
@@ -24,6 +24,29 @@ const UserModel = {
 
         return user;
     },
+    update: async function ({ nickname, profileImage, mbti, religion, height, hobby, personality, ideal, introduce }) {
+        const updateUser = await db.User.update({
+            nickname: nickname,
+            profileImage: profileImage,
+            mbti: mbti,
+            religion: religion,
+            height: height,
+            hobby: hobby,
+            personality: personality,
+            ideal: ideal,
+            introduce: introduce,
+        });
+        return updateUser;
+    },
+    delete: async function (userId) {
+        const deleteUser = await db.User.destory({
+            where: {
+                userId: userId,
+                isDeleted: 0,
+            },
+        });
+        return deleteUser;
+    },
 };
 
 export { UserModel };
