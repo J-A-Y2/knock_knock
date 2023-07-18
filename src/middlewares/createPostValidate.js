60677b3cf27ef07d1a8de6e0abc6c8dbbc7f86e7
[AI7/2반] 이은석 <zhes4593@gmail.com>
2023-07-18 20:21:28 +0900

    Feat: post 작성 validation 추가

---
+++ src/middlewares/createPostValidate.js
@@ -0,0 +1,39 @@
+import { validationResult, body } from 'express-validator';
+import { BadRequestError } from './errorMiddleware.js';
+
+const allowedPostTypes = ['술', '영화', '식사', '카페', '산책', '드라이브', '공연관람', '기타'];
+
+const createPostValidationRules = [
+    body('postTitle')
+        .notEmpty()
+        .withMessage('제목을 입력하세요.')
+        .isLength({ max: 100 })
+        .withMessage('게시글 제목은 100자 이내로 작성해주세요.'),
+    body('postContent')
+        .notEmpty()
+        .withMessage('내용을 입력하세요.')
+        .isLength({ max: 200 })
+        .withMessage('게시글 내용은 200자 이내로 작성해주세요'),
+    body('postType')
+        .notEmpty()
+        .withMessage('모임의 목적을 입력하세요.')
+        .custom(value => {
+            if (!allowedPostTypes.includes(value)) {
+                throw new BadRequestError('유효하지 않은 모임 목적입니다.');
+            }
+        }),
+    body('people').notEmpty().withMessage('모임 인원을 입력하세요.'),
+    body('place').notEmpty().withMessage('모임 장소를 입력하세요.'),
+    body('meetingTime').notEmpty().withMessage('모임 시간을 입력하세요.'),
+];
+
+const createPostValidate = (req, res, next) => {
+    const errors = validationResult(req).errors;
+
+    if (errors.length > 0) {
+        throw new BadRequestError(errors[0].msg);
+    }
+    next();
+};
+
+export { createPostValidate, createPostValidationRules };
