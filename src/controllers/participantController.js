3dd336a3ddea101b200ece3d0ec4cd35c7d8f089
[AI7/2반] 이은석 <zhes4593@gmail.com>
2023-07-20 17:27:45 +0900

    Feat: 모임 참가 신청 수락 구현, 스웨거 수정

--- src/controllers/participantController.js
+++ src/controllers/participantController.js
@@ -35,7 +35,20 @@ const participantController = {
             const participants = await participantService.getParticipants({ userId, postId });
 
             statusCode.setResponseCode200(res);
-            res.send({ message: participants.message, participantsList: participants.participants });
+            res.send({ message: participants.message, total: participants.total, participantsList: participants });
+        } catch (error) {
+            next(error);
+        }
+    },
+    allow: async (req, res, next) => {
+        try {
+            const postId = req.params.postId;
+            const participantId = req.params.participantId;
+
+            const participant = await participantService.allow({ postId, participantId });
+
+            statusCode.setResponseCode200(res);
+            res.send({ message: participant.message });
         } catch (error) {
             next(error);
         }
