b597eb76f8f55abc5bea0245190d93953e78d1a1
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-07-22 14:21:18 +0900

    Feat: 유저 생일을 나이로계산해서 데이터베이스에 넣기

--- src/services/userService.js
+++ src/services/userService.js
@@ -29,6 +29,18 @@ const userService = {
             const hashedPassword = await bcrypt.hash(userInfo.user_password, parseInt(process.env.PW_HASH_COUNT));
             userInfo.user_password = hashedPassword;
 
+            // birthday를 나이로 계산해서 데이터베이스에 넣기
+            const today = new Date();
+            const birthDate = new Date(userInfo.birthday);
+            let age = today.getFullYear() - birthDate.getFullYear();
+            const monthDiff = today.getMonth() - birthDate.getMonth();
+            if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+                age -= 1;
+            }
+
+            // userInfo객체에 age값을 추가하기
+            userInfo.age = age;
+
             const createdUser = await UserModel.create(userInfo);
 
             // 취미 태그 생성
