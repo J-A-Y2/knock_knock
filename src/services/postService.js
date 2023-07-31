b906ced1bc6471ec865d16edffe10c1c368b939a
[AI7/2반] 이은석 <zhes4593@gmail.com>
2023-08-01 02:38:22 +0900

    Feet: 권한 검사하는 공통함수, post 함수 생성 - post Service 수정

--- src/services/postService.js
+++ src/services/postService.js
@@ -3,9 +3,15 @@ import { UserModel } from '../db/models/UserModel.js';
 import { FileModel } from '../db/models/FileModel.js';
 import { ParticipantModel } from '../db/models/ParticipantModel.js';
 import { db } from '../db/index.js';
-import { BadRequestError, InternalServerError, NotFoundError, UnauthorizedError } from '../middlewares/errorMiddleware.js';
-import { setRecruitedValue, fieldsToUpdate } from '../utils/postFunctions.js';
-import { throwNotFoundError } from '../utils/commonFunctions.js';
+import {
+    ConflictError,
+    BadRequestError,
+    InternalServerError,
+    NotFoundError,
+    UnauthorizedError,
+} from '../middlewares/errorMiddleware.js';
+import { setRecruitedValue, fieldsToUpdate, compareTotalRecrutied } from '../utils/postFunctions.js';
+import { checkAccess, throwNotFoundError } from '../utils/commonFunctions.js';
 import { extensionSplit } from '../utils/userFunction.js';
 import { logger } from '../utils/logger.js';
 
@@ -15,10 +21,7 @@ const postService = {
         try {
             const { postImage, ...postInfo } = newPost;
             const user = await UserModel.findById(userId);
-
-            if (!user) {
-                throw new UnauthorizedError('잘못된 토큰입니다.');
-            }
+            throwNotFoundError(user, '유저');
 
             setRecruitedValue(user, postInfo);
 
@@ -73,12 +76,6 @@ const postService = {
         try {
             const post = await PostModel.getPostById(postId);
             throwNotFoundError(post, '게시글');
-            // const userWithImage = {
-            //     nickname: post.User.nickname,
-            //     profileImage: post.User.UserFiles[0].File.url,
-            // };
-
-            // const postImage = post.PostFiles[0]?.File.url || null;
 
             return {
                 message: '게시글 조회를 성공했습니다.',
@@ -97,23 +94,10 @@ const postService = {
         try {
             let post = await PostModel.getPostById(postId);
             throwNotFoundError(post, '게시글');
-
-            if (post.userId !== userId) {
-                throw new UnauthorizedError('수정 권한이 없습니다.');
-            }
+            checkAccess(userId, post.userId, '게시글 수정');
 
             const { postImage, ...updateValue } = toUpdate;
-
-            if (toUpdate.totalM) {
-                if (post.recruitedM > toUpdate.totalM) {
-                    throw new BadRequestError('현재 모집된 인원보다 적게 수정할 수 없습니다.');
-                }
-            }
-            if (toUpdate.totalF) {
-                if (post.recruitedF > toUpdate.totalF) {
-                    throw new BadRequestError('현재 모집된 인원보다 적게 수정할 수 없습니다.');
-                }
-            }
+            compareTotalRecrutied(post, updateValue);
 
             for (const [field, fieldToUpdate] of Object.entries(fieldsToUpdate)) {
                 if (toUpdate[field]) {
@@ -132,6 +116,7 @@ const postService = {
                     transaction,
                 );
             }
+
             if (post.PostFiles.length == 0 && postImage) {
                 const fileExtension = extensionSplit(postImage[1]);
                 await FileModel.createPostImage(
@@ -149,7 +134,7 @@ const postService = {
             if (transaction) {
                 await transaction.rollback();
             }
-            if (error instanceof UnauthorizedError || error instanceof NotFoundError || error instanceof BadRequestError) {
+            if (error instanceof ConflictError || error instanceof NotFoundError || error instanceof BadRequestError) {
                 throw error;
             } else {
                 throw new InternalServerError('게시글 수정을 실패했습니다.');
@@ -161,15 +146,12 @@ const postService = {
             const post = await PostModel.getPostById(postId);
 
             throwNotFoundError(post, '게시글');
-
-            if (post.userId !== userId) {
-                throw new UnauthorizedError('삭제 권한이 없습니다.');
-            }
+            checkAccess(post.userId, userId, '삭제');
 
             await PostModel.delete(postId);
             return { message: '게시글 삭제를 성공했습니다.' };
         } catch (error) {
-            if (error instanceof UnauthorizedError || error instanceof NotFoundError) {
+            if (error instanceof ConflictError || error instanceof NotFoundError) {
                 throw error;
             } else {
                 throw new InternalServerError('게시글 삭제를 실패했습니다.');
